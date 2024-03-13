2f6a9d8e5b850c5c6f9e9892b38673bd8113689c
Sergenti <florian.rieder@unil.ch>
2024-03-13 12:21:47 +0100

    Get cantonal aggregate data

--- data.js
+++ data.js
@@ -28,24 +28,46 @@ canton2id = {
     "valais": 23,
     "neuch_tel": 24,
     "geneva": 25,
-    "jura": 26
-    // "foreign" can also happen
+    "jura": 26,
+    "foreign": 99// "foreign" can also happen
 }
 
+// Number of games per canton
+// aargau: 465
+// appenzell: 217
+// "basel-landschaft": 93
+// "basel-stadt": 248
+// bern: 682
+// biel: 155
+// foreign: 341
+// fribourg: 992
+// geneva: 1612
+// luzern: 496
+// neuch_tel: 248
+// schaffhausen: 93
+// solothurn: 31
+// st_gallen: 155
+// thurgau: 62
+// ticino: 341
+// valais: 155
+// vaud: 3782
+// z_rich: 7347
+// zug: 31
+
 async function getData() {
-
     games = [];
-
-    promises = []
+    cantons = {};
+    promises = [];
 
     // Grab all the data. Since there aren't more specific API endpoints
+    // There's about 31 pages of data
     for (i = 0; i < 32; i++) {
 
-        promise = fetch(`https://api.swissgames.garden/search/games?page=${i}`)
+        promise = fetch(`${API_ENDPOINT}?page=${i}`)
             .then((response) => response.json())
-            .catch((error) => console.log(error))
+            .catch((error) => console.log(error));
 
-        promises.push(promise)
+        promises.push(promise);
     }
 
     // Wait for all requests to be completed
@@ -53,9 +75,24 @@ async function getData() {
 
         for (json of jsons) {
 
-            console.log(json);
+            //console.log(json);
 
-            hits = json["hits"]["hits"];
+            const cantonsAggregate = json["aggregations"]["aggs_all"]["all_filtered_cantons"]["all_nested_cantons"]["cantons_name_keyword"]["buckets"];
+
+            for (obj of cantonsAggregate) {
+                const cantonSlug = obj["key"];
+                const numGames = obj["doc_count"];
+
+                if (cantonSlug in cantons) {
+                    // Add to the total number of games for that canton
+                    cantons[cantonSlug] += numGames;
+                } else {
+                    // Set the total number of games for that canton
+                    cantons[cantonSlug] = numGames
+                }
+            }
+
+            const hits = json["hits"]["hits"];
 
             if (hits.length == 0) {
                 console.log('EMPTY');
@@ -66,31 +103,38 @@ async function getData() {
                 games.push(hit["_source"]);
             }
         }
-    })
+    });
 
 
     console.log(games);
-    return games;
+    console.log(cantons);
+    return { games, cantons };
 }
 
-function storeLocalData(key, data){
+function storeLocalData(key, data) {
     // Put the object into storage
     localStorage.setItem(key, JSON.stringify(data));
 }
-function retrieveLocalData(key){
+function retrieveLocalData(key) {
     return JSON.parse(localStorage.getItem(key));
 }
 
-async function getCachedData(){
-    if (localStorage.getItem("data") === null) {
+async function getCachedData() {
+    if (localStorage.getItem("games") === null) {
         data = await getData();
 
-        storeLocalData("data", data);
+        storeLocalData("games", data.games);
+        storeLocalData("cantons", data.cantons);
     } else {
-        data = retrieveLocalData("data");
+        games = retrieveLocalData("games");
+        cantons = retrieveLocalData("cantons");
+        data = { games, cantons };
     }
 
     return data;
 }
 
-getCachedData().then((data) => console.log(data))
+getCachedData().then((data) => {
+    console.log(data.games);
+    console.log(data.cantons);
+});
