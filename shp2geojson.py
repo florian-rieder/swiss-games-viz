e533fe40650a1a744853275c0fce0934b06cd862
Sergenti <florian.rieder@unil.ch>
2024-03-13 14:23:56 +0100

    Merged Appenzell

--- shp2geojson.py
+++ shp2geojson.py
@@ -3,7 +3,7 @@ Convert shapefile into GeoJSON and convert the coordinate system from
 LV95 (Swiss national coordinate system) to WGS84 (global coordinate
 system).
 """
-
+import pandas as pd
 import geopandas as gpd
 
 # Read the shapefile
@@ -15,6 +15,26 @@ gdf['geometry'] = gdf['geometry'].simplify(tolerance)
 
 # Reproject to WGS84 (EPSG:4326)
 gdf_wgs84 = gdf.to_crs("EPSG:4326")
+
+# Merge Appenzell Innerrhoden and Appenzell Ausserrhoden
+regions_to_merge = gdf_wgs84[gdf_wgs84['id'].isin([15, 16])]
+
+# Combine the geometries of the selected regions into a single geometry
+merged_geometry = regions_to_merge.unary_union
+
+# Create a new GeoDataFrame with the merged geometry
+merged_region = gpd.GeoDataFrame(geometry=[merged_geometry])
+
+# Set the name and id of the merged region
+merged_region['name'] = 'Appenzell'
+merged_region['id'] = 15
+
+# Drop the original regions from the GeoDataFrame
+gdf_wgs84 = gdf_wgs84.drop(gdf_wgs84[gdf_wgs84['id'].isin([15, 16])].index)
+
+# Append the merged region to the GeoDataFrame
+gdf_wgs84 = pd.concat([gdf_wgs84, merged_region], ignore_index=True)
+
 
 # Save as GeoJSON
 gdf_wgs84.to_file('shapefiles/output.geojson', driver='GeoJSON')
